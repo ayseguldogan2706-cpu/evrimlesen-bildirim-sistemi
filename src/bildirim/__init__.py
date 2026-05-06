7b8ea76a4a35b78c4b6846c1e75c67af4562c314
Ayşegül Doğan <248220958+ayseguldogan2706-cpu@users.noreply.github.com>
2026-05-06 13:33:03 +0300

    feat: Observer ile gonderim olaylarini yayinla

--- src/bildirim/__init__.py
+++ src/bildirim/__init__.py
@@ -2,11 +2,15 @@ from .basit_sistem import BildirimSistemi
 from .fabrika import BildirimFabrikasi
 from .merkez import BildirimMerkezi
 from .modeller import BildirimIstegi, GonderimSonucu
+from .olaylar import BildirimOlayi, GonderimGecmisi, OlayYayinci
 
 __all__ = [
     "BildirimSistemi",
     "BildirimMerkezi",
     "BildirimFabrikasi",
     "BildirimIstegi",
+    "BildirimOlayi",
+    "GonderimGecmisi",
     "GonderimSonucu",
+    "OlayYayinci",
 ]
--- src/bildirim/basit_sistem.py
+++ src/bildirim/basit_sistem.py
@@ -1,11 +1,15 @@
 from .fabrika import BildirimFabrikasi
 from .modeller import BildirimIstegi, GonderimSonucu
+from .olaylar import BildirimOlayi, GonderimGecmisi, OlayYayinci
 
 
 class BildirimSistemi:
-    def __init__(self, fabrika=None):
-        self.gonderim_gecmisi = []
+    def __init__(self, fabrika=None, olay_yayinci=None):
         self.fabrika = fabrika or BildirimFabrikasi()
+        self.olay_yayinci = olay_yayinci or OlayYayinci()
+        self.gecmis_dinleyicisi = GonderimGecmisi()
+        self.olay_yayinci.dinleyici_ekle(self.gecmis_dinleyicisi)
+        self.gonderim_gecmisi = self.gecmis_dinleyicisi.kayitlar
 
     def bildirim_gonder(self, kanal, alici, mesaj, oncelik="normal"):
         istek = BildirimIstegi(kanal=kanal, alici=alici, mesaj=mesaj, oncelik=oncelik)
@@ -14,8 +18,14 @@ class BildirimSistemi:
             sonuc = kanal_nesnesi.gonder(istek)
         except ValueError as hata:
             sonuc = GonderimSonucu.olustur(istek, False, str(hata))
-        self.gonderim_gecmisi.append(sonuc)
+        self.olay_yayinci.yayinla(BildirimOlayi("gonderim_tamamlandi", sonuc))
         return sonuc
+
+    def dinleyici_ekle(self, dinleyici):
+        self.olay_yayinci.dinleyici_ekle(dinleyici)
+
+    def dinleyici_cikar(self, dinleyici):
+        self.olay_yayinci.dinleyici_cikar(dinleyici)
 
     def toplu_bildirim_gonder(self, kanal, alicilar, mesaj, oncelik="normal"):
         sonuclar = []
